8adcd815cce67d2bea1a15a21f9a9419f502d9d4
Anupam Singh <anupamsingh4440@gmail.com>
2026-09-09 16:16:04 +0530

    Allowed Host

--- SBM/settings.py
+++ SBM/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-e63%0o&+7tgi@vtdj$$t)x1uma%myhklh0=f@$&uqchr5fn9_n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://smartbuildhub.up.railway.app/']
 
 
 # Application definition
